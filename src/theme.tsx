43cea915903cca9f10c3db8798e026160d23c206
Michael <michael2ga3@gmail.com>
2023-02-26 09:20:15 +0300

    Green theme for green colledge

---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+import { green, red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: green[900],
+    },
+    secondary: {
+      main: red[500],
+    },
+  },
+})
+
+export default theme;
